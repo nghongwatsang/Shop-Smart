6fbaa8513fb6b59c8799c74cc8efaa7f4a99270d
WBroadwell <broadw@rpi.edu>
2025-10-22 22:58:47 -0400

    new file:   frontend/app/context/GlobalContext.tsx 	modified:   frontend/app/layout.tsx 	modified:   frontend/components/CartMenu.tsx

--- frontend/components/CartMenu.tsx
+++ frontend/components/CartMenu.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from "next/link";
+import { useGlobal } from '../app/context/GlobalContext';
 
 // Cart extension feature, not finished yet
 
 export default function CartMenu() {
+    const { shoppingList, setShoppingList } = useGlobal();
     const [visible, setVisible] = useState(false);
 
     return (
@@ -15,14 +18,21 @@ export default function CartMenu() {
             {visible && (
                 // Needs work here, maybe use Sidebar object from Shadcn
                 <>
-                <div>
-                    <div className={`flex flex-col fixed right-0 w-1/5 shadow-xl transition duration-2000 ease-in-out ${visible ? "right-0" : "right-36"}`}>
-                        <button className="bg-green-100 p-1"> Cart </button>
-                        <button className="bg-green-100 p-1"> Cart </button>
-                        <button className="bg-green-100 p-1"> Cart </button>
-                        <button className="bg-green-100 p-1"> Cart </button>
-                        <button className="p-1"> Cart </button>
+                <div className={`w-full flex flex-col not-last-of-type:shadow-xl transition duration-2000 ease-in-out bg-green-100 ${visible ? "right-0" : "right-36"}`}>
+                    <div className="flex flex-col p-2">
+                        {/* Render Elements of shoppingList, not certain whats gonna be in it yet or our plan for formatting so ill wait*/}
+                        {shoppingList.map((element, index) => (
+                            <div key={index} className="flex flex-row items-center p-2 m-2 border-b-2 border-green-300">
+                                <Image src={element.imgSrc} alt={element.item} width={5} height={5} className="mr-2"/>
+                                <div>{element.item}</div>
+                            </div>
+                        ))}
                     </div>
+                    
+                    <button className="bg-blue-300 p-2 m-2 rounded-2xl hover:brightness-90 transition text-center" onClick={() => setShoppingList([])}>Reset Cart</button>
+                    <Link href="/results" className="flex items-center justify-center">
+                        <button className="bg-green-300 p-2 m-2 rounded-2xl hover:brightness-90 transition items-center justify-center"> Search! </button>
+                    </Link>
                 </div>
                 </>
             )}
