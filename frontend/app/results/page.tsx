b6cc299df6578ac762fd31504b26cf028a08aa49
tomeravgil <tomeravgil@gmail.com>
2025-10-29 17:45:54 -0400

    added back button

--- frontend/app/results/page.tsx
+++ frontend/app/results/page.tsx
@@ -7,10 +7,13 @@ import {
   AccordionTrigger,
   AccordionContent,
 } from "@/components/ui/accordion";
+import {useRouter} from "next/navigation";
+import { ArrowLeft } from "lucide-react";
+import GoBackButton from "@/components/back-button";
 
 export default function ResultsPage() {
   const { shoppingList } = useGlobal();
-
+  const router = useRouter();
   function fetchResults() {
     // fetch results from backend using the shoppingList
     return [
@@ -43,37 +46,41 @@ export default function ResultsPage() {
   return (
     <section className="flex flex-col items-center justify-center h-screen w-screen">
       <div className="p-5 text-lg font-medium">Results for basket:</div>
-      <Accordion
-        type="single"
-        collapsible
-        className="w-full max-w-md flex flex-col space-y-2"
-      >
-        {results.map((element, index) => (
-          <AccordionItem
-            value={String(index)}
-            key={index}
-            className="border rounded-lg shadow-sm bg-gray-50 dark:bg-gray-800"
-          >
-            <AccordionTrigger className="flex flex-row justify-between items-center px-6 py-3 text-lg font-medium text-center">
-              <div className="flex-1 text-left">{element.name}</div>
-              <div className="flex-1 text-center">{element.distance}</div>
-              <div className="flex-1 text-right">${element.cost}</div>
-            </AccordionTrigger>
+      {/* Go Back Button */}
+      <GoBackButton router={router} />
 
-            <AccordionContent className="flex flex-col px-6 pb-3">
-              {element.basket.map((sub_element, sub_index) => (
-                <div
-                  className="flex flex-row justify-between py-1 text-sm"
-                  key={sub_index}
-                >
-                  <div>{sub_element.item}</div>
-                  <div>${sub_element.cost}</div>
-                </div>
-              ))}
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+        {/* Accordion */}
+        <Accordion
+          type="multiple"
+          defaultValue={results.map((_, index) => String(index))}
+          className="w-full max-w-md flex flex-col space-y-2"
+        >
+          {results.map((element, index) => (
+            <AccordionItem
+              value={String(index)}
+              key={index}
+              className="border rounded-lg shadow-sm bg-gray-50 dark:bg-gray-800"
+            >
+              <AccordionTrigger className="flex flex-row justify-between items-center px-6 py-3 text-lg font-medium text-center">
+                <div className="flex-1 text-left">{element.name}</div>
+                <div className="flex-1 text-center">{element.distance}</div>
+                <div className="flex-1 text-right">${element.cost}</div>
+              </AccordionTrigger>
+
+              <AccordionContent className="flex flex-col px-6 pb-3">
+                {element.basket.map((sub_element, sub_index) => (
+                  <div
+                    className="flex flex-row justify-between py-1 text-sm"
+                    key={sub_index}
+                  >
+                    <div>{sub_element.item}</div>
+                    <div>${sub_element.cost}</div>
+                  </div>
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
     </section>
   );
 }
