41a60f5df4789c8b117917c6fe71e1e4fe24a68a
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2025-12-09 18:43:56 -0500

    finished docker compose

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "standalone",
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
